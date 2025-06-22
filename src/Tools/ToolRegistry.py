bd370d14c94accc659b52b2cca30ecd1b6c5bb0a
Keanu Interone <keanu.interone@gmail.com>
2025-06-22 14:03:05 +1000

    Added memory tool set

--- src/Tools/ToolRegistry.py
+++ src/Tools/ToolRegistry.py
@@ -10,8 +10,17 @@ from Tools.JiraTools import (
     jira_get_sprint_issues_tool,
 )
 
+from Tools.MemoryTools.append_memory import append_memory_tool
+from Tools.MemoryTools.read_memory import read_memory_tool
+from Tools.MemoryTools.view_memory_shape import view_memory_shape_tool
+from Tools.MemoryTools.delete_memory import delete_memory_tool
+from Tools.MemoryTools.write_memory import write_memory_tool
+
 tool_registry = {
+    # Pass Event Tool
     "pass_event": pass_event_tool,
+
+    # Jira Tools
     "jira_create_issue": jira_create_issue_tool,
     "jira_search_issues": jira_search_issues_tool,
     "jira_update_issue": jira_update_issue_tool,
@@ -20,4 +29,11 @@ tool_registry = {
     "jira_unassign_issue": jira_unassign_issue_tool,
     "jira_get_sprints": jira_get_sprints_tool,
     "jira_get_sprint_issues": jira_get_sprint_issues_tool,
+
+    # Memory Tools
+    "append_memory": append_memory_tool,
+    "read_memory": read_memory_tool,
+    "view_memory_shape": view_memory_shape_tool,
+    "delete_memory": delete_memory_tool,
+    "write_memory": write_memory_tool,  
 }
